b42747a42c54c47144fa7b4806b61988db599341
Agatsya_pc <20agatsyadav03@gmail.com>
2023-11-10 23:23:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
-function App() {
+const App = () => {
+  const [startDate, setStartDate] = useState(null);
+  const [age, setAge] = useState(null);
+
+  const calculateAge = () => {
+    if (startDate) {
+      const currentYear = new Date().getFullYear();
+      const enteredYear = startDate.getFullYear();
+      const calculatedAge = currentYear - enteredYear;
+      setAge(calculatedAge);
+    }
+  };
+
+  const commonStyles = {
+    fontSize: '20px', 
+    textAlign: 'center',
+    margin: '10px',
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <div style={{ ...commonStyles }}>
+        <h1 style={{ fontSize: '36px' }}>Age Calculator</h1>
+        <br />
+        <br />
+        <h2 style={{ fontSize: '24px' }}>Enter your Age here</h2>
+        <br />
+        <DatePicker
+          selected={startDate}
+          onChange={(date) => setStartDate(date)}
+          dateFormat="dd-MM-yyyy"
+          placeholderText="Select your birthdate"
+          style={{ width: '200px', fontSize: '20px' }} 
+        />
+        <br />
+        <br />
+        <button style={{ fontSize: '20px' }} onClick={calculateAge}>
+          Calculate Age
+        </button>
+        <br />
+        <br />
+        {age !== null && <p style={{ fontSize: '24px' }}>Your age is: {age} years</p>}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
